9a9885838b31414bf2cf3e014b5b84819a86c54b
pedrochatelain <pedrochatelain@gmail.com>
2025-04-22 18:38:05 -0300

    navigate to home when login is successful

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 /**
  * @title Basic Inputs
@@ -17,11 +18,13 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({providedIn: 'root'})
 export class LoginComponent {
   private http = inject(HttpClient);
-
   username = ""
   password = ""
   loading = false;
   loggedIn = false;
+  error = null
+
+  constructor(private router: Router) {}
 
   updateUsername(): void {
     console.log(this.username)
@@ -33,13 +36,24 @@ export class LoginComponent {
 
   login(): void {
     this.loading = true
-    setTimeout(() => {
-      this.loading = false
-      this.loggedIn = true
-    }, 3000);
-    this.http.get<any>('http://localhost:3000/api/products', {observe: 'response'}).subscribe(res => {
-      console.log('Response status:', res.status);
-      console.log('Body:', res.body);
+    this.error = null
+    const data = {
+      "username": this.username,
+      "password": this.password
+    }
+    this.http.post<any>('http://192.168.0.149:3000/api/login', data).subscribe({
+      next: response => {
+        console.log('Success:', response);
+        this.router.navigate(['home'])
+      },
+      error: error => {
+        setTimeout(() => this.error = error.error.error, 1000);
+        console.error('Errore:', error);
+      },
+      complete: () => {
+        console.log('Request complete');
+      }
     });
+    setTimeout(() => this.loading = false, 1000);
   }
 }
