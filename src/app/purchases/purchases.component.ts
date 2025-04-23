dda6271e2bdfb0f4509150b5b1fa0ad6515bf5a6
pedrochatelain <pedrochatelain@gmail.com>
2025-04-23 16:23:44 -0300

    get user purchases

--- src/app/purchases/purchases.component.ts
+++ src/app/purchases/purchases.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardProductComponent } from '../card-product/card-product.component';
 
 @Component({
   selector: 'app-purchases',
-  imports: [],
+  imports: [CardProductComponent],
   templateUrl: './purchases.component.html',
   styleUrl: './purchases.component.css'
 })
+@Injectable({ providedIn: 'root' })
 export class PurchasesComponent {
+  private http = inject(HttpClient);
+  purchases: Purchase[] = []; // Updated to an array
+  userId: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token'); // Replace 'token' with the actual key used to store your token
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+    this.userId = this.route.snapshot.paramMap.get('id_user');
+    this.http.get<Purchase[]>(`http://192.168.0.149:3000/api/purchases/${this.userId}`, { headers }).subscribe({
+      next: (response) => {
+        setTimeout(() => {
+          this.purchases = response;
+          console.log('Success:', response);
+        }, 2000);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request complete');
+      },
+    });
+  }
+}
+
+
+interface Purchase {
+  _id: string;
+  products: Product[];
+}
+
+interface Product {
+  _id: string;
+  name: string;
+  price: number;
 }
