02f453734978b3688d242f923072fe75fac810df
pedrochatelain <pedrochatelain@gmail.com>
2025-04-23 13:32:41 -0300

    allow logged in user to navigate to purchases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PurchasesComponent } from './purchases/purchases.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default route redirects to login
     { path: 'login', component: LoginComponent },         // Login route  
     { path: 'home', component: HomeComponent },
+    { path: 'purchases/:id_user', component: PurchasesComponent, canActivate: [AuthGuard] },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, Injectable, OnInit } from '@angular/core';
 import {CardProductComponent} from '../card-product/card-product.component'
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,34 @@ export class HomeComponent implements OnInit {
   private http = inject(HttpClient);
   loading = true;
   products: any[] = [];
+
+  constructor(private router: Router) {}
+
+  navigateToPurchases(): void {
+    const token = localStorage.getItem('token'); // Replace 'token' with the actual key you use.
+    if (token) {
+      const decodedToken = this.decodeToken(token);
+      const userId = decodedToken?.id; // Replace 'user_id' with the actual key in the payload.
+      if (userId) {
+        this.router.navigate([`purchases/${userId}`]);
+      } else {
+        console.error('User ID not found in the token.');
+      }
+    } else {
+      console.error('No token found in localStorage.');
+      this.router.navigate(['/login']);
+    }
+  }
+
+  decodeToken(token: string): any {
+    try {
+      const payload = token.split('.')[1];
+      return JSON.parse(atob(payload));
+    } catch (error) {
+      console.error('Failed to decode token:', error);
+      return null;
+    }
+  }
 
   ngOnInit(): void {
     this.http.get<any>('http://192.168.0.149:3000/api/products').subscribe({
