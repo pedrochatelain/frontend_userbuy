5c8ca1a8e2a8f70505f6bfbdf4e2a2b0b8995b05
pedrochatelain <pedrochatelain@gmail.com>
2025-04-23 16:40:47 -0300

    add navbar to all routes except login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component'
+import { Router, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-userbuy';
+  showNavBar: boolean = true;
+
+  constructor(private router: Router) {
+    // Subscribe to route changes to show/hide the nav-bar
+    this.router.events.subscribe(() => {
+      const currentRoute = this.router.url;
+      this.showNavBar = currentRoute !== '/login'; // Hide nav-bar on login page
+    });
+  }
 }
