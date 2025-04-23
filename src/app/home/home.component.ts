8c450e0f62ba77d0bfcd3d9b21bd8ece7fcbe1f4
pedrochatelain <pedrochatelain@gmail.com>
2025-04-22 21:46:11 -0300

    load products when endpoint /api/home is hit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable, OnInit } from '@angular/core';
+import {CardProductComponent} from '../card-product/card-product.component'
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+@Injectable({providedIn: 'root'})
+export class HomeComponent implements OnInit {
+
+  private http = inject(HttpClient);
+  loading = true;
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>('http://192.168.0.149:3000/api/products').subscribe({
+      next: response => {
+        setTimeout(() => {
+          this.loading = false
+          this.products = response
+          console.log('Success:', response);
+          
+        }, 2000);
+      },
+      error: error => {
+        console.error('Errore:', error);
+      },
+      complete: () => {
+        console.log('Request complete');
+      }
+    });
+  }
 
 }
