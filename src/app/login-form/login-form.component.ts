9c3db9b1d827e39005d6da356ab144c85985ed49
pedrochatelain <pedrochatelain@gmail.com>
2025-04-24 17:09:29 -0300

    extract login form to own component

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,53 @@
+import {Component, inject, Injectable, OnInit} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+@Injectable({providedIn: 'root'})
+export class LoginFormComponent implements OnInit {
+  private http = inject(HttpClient);
+  username = ""
+  password = ""
+  loading = false;
+  loggedIn = false;
+  error = null
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.username = ""
+    this.password = ""
+  }
+
+  login(): void {
+    this.loading = true
+    this.error = null
+    const data = {
+      "username": this.username,
+      "password": this.password
+    }
+    this.http.post<any>('http://192.168.0.149:3000/api/login', data).subscribe({
+      next: response => {
+        console.log('Success:', response);
+        this.router.navigate(['home'])
+        localStorage.setItem('token', response.token);
+      },
+      error: error => {
+        this.error = error.error.error
+        console.error('Errore:', error);
+      },
+      complete: () => {
+        console.log('Request complete');
+      }
+    });
+    this.loading = false
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,5 @@
-import {Component, inject, Injectable, OnInit} from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 /**
  * @title Basic Inputs
@@ -13,44 +8,8 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   styleUrl: 'login.component.css',
   templateUrl: 'login.component.html',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [LoginFormComponent],
 })
-@Injectable({providedIn: 'root'})
-export class LoginComponent implements OnInit {
-  private http = inject(HttpClient);
-  username = ""
-  password = ""
-  loading = false;
-  loggedIn = false;
-  error = null
-
-  constructor(private router: Router) {}
-  ngOnInit(): void {
-    this.username = ""
-    this.password = ""
-  }
-
-  login(): void {
-    this.loading = true
-    this.error = null
-    const data = {
-      "username": this.username,
-      "password": this.password
-    }
-    this.http.post<any>('http://192.168.0.149:3000/api/login', data).subscribe({
-      next: response => {
-        console.log('Success:', response);
-        this.router.navigate(['home'])
-        localStorage.setItem('token', response.token);
-      },
-      error: error => {
-        this.error = error.error.error
-        console.error('Errore:', error);
-      },
-      complete: () => {
-        console.log('Request complete');
-      }
-    });
-    this.loading = false
-  }
+export class LoginComponent {
+  
 }
