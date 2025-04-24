7b0b6352007c913d2a973b3fdcda8a1d64ba48a8
pedrochatelain <pedrochatelain@gmail.com>
2025-04-24 11:59:07 -0300

    refactor product card

--- src/app/card-product/card-product.component.ts
+++ src/app/card-product/card-product.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-product',
-  imports: [MatCardModule, MatChipsModule],
+  imports: [MatCardModule, MatChipsModule, MatButtonModule, CommonModule],
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.css'
 })
