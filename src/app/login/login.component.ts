367a87840bf9e77143bd9cf4616a09855e7e14dd
pedrochatelain <pedrochatelain@gmail.com>
2025-04-25 14:43:01 -0300

    add create account to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../login-form/login-form.component';
+import { CreateAccountFormComponent } from '../create-account-form/create-account-form.component';
+import { MatButtonModule } from '@angular/material/button';
 
 /**
  * @title Basic Inputs
@@ -8,8 +10,13 @@ import { LoginFormComponent } from '../login-form/login-form.component';
   selector: 'app-login',
   styleUrl: 'login.component.css',
   templateUrl: 'login.component.html',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, CreateAccountFormComponent, MatButtonModule],
 })
 export class LoginComponent {
-  
+  showLoginForm = true;
+  toggle_forms_text = "I don't have an account"
+
+  toggleForms() {
+    this.showLoginForm = !this.showLoginForm;
+  }  
 }
