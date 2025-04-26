d03de9e49291f815ed24407373f8088be2efa60d
pedrochatelain <pedrochatelain@gmail.com>
2025-04-26 13:40:32 -0300

    add functionality create user account

--- src/app/create-account-form/create-account-form.component.ts
+++ src/app/create-account-form/create-account-form.component.ts
@@ -1,10 +1,12 @@
-import {Component, inject, Injectable, OnInit} from '@angular/core';
+import {Component, inject, Injectable, Input, OnInit} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
+import { SnackbarComponent } from '../snackbar/snackbar.component';
 
 @Component({
   selector: 'app-create-account-form',
@@ -17,37 +19,60 @@ export class CreateAccountFormComponent implements OnInit {
   private http = inject(HttpClient);
   username = ""
   password = ""
+  repeat_password = ""
   loading = false;
   loggedIn = false;
   error = null
+  private _snackBar = inject(MatSnackBar);
 
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.username = ""
     this.password = ""
+    this.repeat_password = ""
   }
 
   createAccount(): void {
-    // this.loading = true
-    // this.error = null
-    // const data = {
-    //   "username": this.username,
-    //   "password": this.password
-    // }
-    // this.http.post<any>('http://192.168.0.149:3000/api/login', data).subscribe({
-    //   next: response => {
-    //     console.log('Success:', response);
-    //     this.router.navigate(['home'])
-    //     localStorage.setItem('token', response.token);
-    //   },
-    //   error: error => {
-    //     this.error = error.error.error
-    //     console.error('Errore:', error);
-    //   },
-    //   complete: () => {
-    //     console.log('Request complete');
-    //   }
-    // });
-    // this.loading = false
+    if (this.password === this.repeat_password) {
+      this.loading = true
+      this.error = null
+      const data = {
+        "username": this.username,
+        "password": this.password
+      }
+      this.http.post<any>('http://192.168.0.149:3000/api/users', data).subscribe({
+        next: response => {
+          this.username = ""
+          this.password = ""
+          this.repeat_password = ""
+          this.openSnackBar("User created successfully", "Close", false)
+        },
+        error: error => {
+          this.error = error.error.error
+          console.error('Errore:', error);
+        },
+        complete: () => {
+          console.log('Request complete');
+        }
+      });
+      this.loading = false
+    } else {
+      this.openSnackBar("Couldn't create user", "Close", true)
+    }
   }
+
+  openSnackBar(message: string, action: string, hasError: boolean) {
+    let config = new MatSnackBarConfig();
+    config.announcementMessage = message
+    config.duration = 3000;
+    config.horizontalPosition = 'end';
+    config.verticalPosition = 'bottom'
+    config.panelClass = ['snackbar']
+    if (hasError) {
+      config.panelClass = ['red-snackbar']
+    }
+    // this._snackBar.open(message, action, config);
+    this._snackBar.openFromComponent(SnackbarComponent, config)
+  }
+
 }
