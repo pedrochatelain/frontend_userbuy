a1b6396c079dd27c24ac3de629fcd9fc15b5091f
pedrochatelain <pedrochatelain@gmail.com>
2025-06-03 18:03:03 -0300

    create component dialog-add-product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,11 @@ import { Router, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {inject} from '@angular/core';
-import {
-  MatDialog,
-  MAT_DIALOG_DATA,
-  MatDialogTitle,
-  MatDialogContent,
-} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { AuthGuard } from './auth/auth.guard';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { DialogAddProductComponent } from './dialog-add-product/dialog-add-product.component';
 
 @Component({
   selector: 'app-root',
@@ -37,98 +28,11 @@ export class AppComponent {
   }
 
   openDialog() {
-    this.dialog.open(DialogDataExampleDialog, {
-      data: {
-        animal: 'panda',
-      },
-    });
+    this.dialog.open(DialogAddProductComponent);
   }
 
   get isAdmin(): boolean {
     return this.authGuard.isAdmin();
   }
 
-}
-
-export interface DialogData {
-  animal: 'panda' | 'unicorn' | 'lion';
-}
-
-@Component({
-  selector: 'dialog-data-example-dialog',
-  templateUrl: './dialog-data-example-dialog.html',
-  styleUrl: './dialog-data-example-dialog.css',
-  imports: [MatDialogTitle, MatDialogContent, MatInputModule, FormsModule, MatFormFieldModule, MatButtonModule, CommonModule, MatIconModule, MatProgressSpinnerModule],
-})
-export class DialogDataExampleDialog {
-  data = inject(MAT_DIALOG_DATA);
-  name = ""
-  category = ""
-  stock = ""
-  currency = ""
-  price = ""
-  btnTitle = "Add Product"
-  private http = inject(HttpClient);
-  error = ""
-  errorName = ""
-  errorCategory = ""
-  success = ""
-  isBtnDisabled = false
-  productAdded = false;
-  product!: Product;
-  loading = false
-
-  addProduct(): void {
-    this.loading = true
-    this.error = ""
-    this.errorName = ""
-    this.errorCategory = ""
-    const product = {
-      "category": this.category,
-      "price": this.price,
-      "name": this.name,
-      "stock_quantity": this.stock,
-      "currency": this.currency
-    };
-    this.productAdded = false
-
-    // Retrieve the token from local storage
-    const token = localStorage.getItem('token');
-
-    // Set up headers with the token
-    const headers = { 
-      'Authorization': `Bearer ${token}` 
-    };
-
-    // Make the HTTP POST request with headers
-    this.http.post<any>('http://192.168.0.149:3000/api/products', product, { headers }).subscribe({
-      next: response => {
-        this.product = response.product
-        this.productAdded = true
-        this.success = "Product added successfully"
-        this.btnTitle = "Add Product"
-        this.isBtnDisabled = true
-        this.loading = false
-      },
-      error: error => {
-        console.error('Error:', error);
-        this.error = error.error.error
-        this.errorName = error.error.issues.name
-        this.errorCategory = error.error.issues.category
-        this.btnTitle = "Add Product"
-        this.loading = false
-      },
-      complete: () => {
-      }
-    });
-  }
-
-}
-
-interface Product {
-  category: String,
-  price: String,
-  name: String,
-  stock_quantity: String,
-  currency: String
 }
---
+++ src/app/dialog-add-product/dialog-add-product.component.ts
@@ -0,0 +1,99 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-dialog-add-product',
+  imports: [
+    MatDialogTitle,
+    MatDialogContent, 
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    CommonModule,
+    MatIconModule, 
+    MatProgressSpinnerModule
+  ],
+  templateUrl: './dialog-add-product.component.html',
+  styleUrl: './dialog-add-product.component.css'
+})
+export class DialogAddProductComponent {
+data = inject(MAT_DIALOG_DATA);
+  name = ""
+  category = ""
+  stock = ""
+  currency = ""
+  price = ""
+  btnTitle = "Add Product"
+  private http = inject(HttpClient);
+  error = ""
+  errorName = ""
+  errorCategory = ""
+  success = ""
+  isBtnDisabled = false
+  productAdded = false;
+  product!: Product;
+  loading = false
+
+  addProduct(): void {
+    this.loading = true
+    this.error = ""
+    this.errorName = ""
+    this.errorCategory = ""
+    const product = {
+      "category": this.category,
+      "price": this.price,
+      "name": this.name,
+      "stock_quantity": this.stock,
+      "currency": this.currency
+    };
+    this.productAdded = false
+
+    // Retrieve the token from local storage
+    const token = localStorage.getItem('token');
+
+    // Set up headers with the token
+    const headers = { 
+      'Authorization': `Bearer ${token}` 
+    };
+
+    // Make the HTTP POST request with headers
+    this.http.post<any>('http://192.168.0.149:3000/api/products', product, { headers }).subscribe({
+      next: response => {
+        this.product = response.product
+        this.productAdded = true
+        this.success = "Product added successfully"
+        this.btnTitle = "Add Product"
+        this.isBtnDisabled = true
+        this.loading = false
+      },
+      error: error => {
+        console.error('Error:', error);
+        this.error = error.error.error
+        this.errorName = error.error.issues.name
+        this.errorCategory = error.error.issues.category
+        this.btnTitle = "Add Product"
+        this.loading = false
+      },
+      complete: () => {
+      }
+    });
+  }
+
+}
+
+interface Product {
+  category: String,
+  price: String,
+  name: String,
+  stock_quantity: String,
+  currency: String
+}
