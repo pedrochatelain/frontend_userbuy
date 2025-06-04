e4fa9391fddbceb264c5135bc255231330adab27
pedrochatelain <pedrochatelain@gmail.com>
2025-06-04 17:49:53 -0300

    use product service in dialog add product

--- src/app/dialog-add-product/dialog-add-product.component.ts
+++ src/app/dialog-add-product/dialog-add-product.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { ProductService, Product } from '../product.service';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -33,7 +33,6 @@ data = inject(MAT_DIALOG_DATA);
   currency = ""
   price = ""
   btnTitle = "Add Product"
-  private http = inject(HttpClient);
   error = ""
   errorName = ""
   errorCategory = ""
@@ -43,6 +42,7 @@ data = inject(MAT_DIALOG_DATA);
   product!: Product;
   loading = false
 stockError = '';
+  private productService = inject(ProductService);
 
 validateStock() {
   const stockValue = this.stock; // Get the current stock value as a string
@@ -79,43 +79,23 @@ validateStock() {
     };
     this.productAdded = false
 
-    // Retrieve the token from local storage
-    const token = localStorage.getItem('token');
-
-    // Set up headers with the token
-    const headers = { 
-      'Authorization': `Bearer ${token}` 
-    };
-
-    // Make the HTTP POST request with headers
-    this.http.post<any>('http://192.168.0.149:3000/api/products', product, { headers }).subscribe({
-      next: response => {
-        this.product = response.product
-        this.productAdded = true
-        this.success = "Product added successfully"
-        this.btnTitle = "Add Product"
-        this.isBtnDisabled = true
-        this.loading = false
+    this.productService.addProduct(product).subscribe({
+      next: (response) => {
+        this.product = response.product;
+        this.productAdded = true;
+        this.success = 'Product added successfully';
+        this.btnTitle = 'Add Product';
+        this.isBtnDisabled = true;
+        this.loading = false;
       },
-      error: error => {
+      error: (error) => {
         console.error('Error:', error);
-        this.error = error.error.error
-        this.errorName = error.error.issues.name
-        this.errorCategory = error.error.issues.category
-        this.btnTitle = "Add Product"
-        this.loading = false
+        this.error = error.error.error;
+        this.errorName = error.error.issues?.name;
+        this.errorCategory = error.error.issues?.category;
+        this.btnTitle = 'Add Product';
+        this.loading = false;
       },
-      complete: () => {
-      }
     });
   }
-
-}
-
-interface Product {
-  category: String,
-  price: String,
-  name: String,
-  stock_quantity: String,
-  currency: String
 }
