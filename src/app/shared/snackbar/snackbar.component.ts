1487cd098854dbbfbbbe0ff53187b8f5f3654bdc
pedrochatelain <pedrochatelain@gmail.com>
2025-06-13 14:26:42 -0300

    refactor snackbar

--- src/app/shared/snackbar/snackbar.component.ts
+++ src/app/shared/snackbar/snackbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, Inject, inject, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarRef } from '@angular/material/snack-bar';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar',
@@ -10,6 +11,16 @@ import { MatSnackBarRef } from '@angular/material/snack-bar';
   styleUrl: './snackbar.component.css'
 })
 export class SnackbarComponent {
-  snackBarRef = inject(MatSnackBarRef);
+  @Input() icon: string = 'task_alt';
+  @Input() hasError: boolean = false;
+  @Input() message: string = '';
+  snackBarRef = inject(MatSnackBarRef);  
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {
+    this.icon = data.icon || this.icon;
+    this.message = data.message || this.message;
+    this.hasError = data.hasError || this.hasError
+  }
+
 }
 
