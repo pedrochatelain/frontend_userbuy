7df692a975a9cb351070299e3a573e6db9c8a0ed
pedrochatelain <pedrochatelain@gmail.com>
2025-06-18 11:28:52 -0300

    remove button purchase and allow navigation when tapping card

--- src/app/features/product/components/card-product/card-product.component.ts
+++ src/app/features/product/components/card-product/card-product.component.ts
@@ -1,32 +1,28 @@
-import { Component, inject, Injectable, Input } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, Injectable, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../../environments/environment';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { ScreenService } from '../../../../shared/services/screen.service';
-import { SnackbarService } from '../../../../shared/snackbar/services/snackbar.service';
+import { Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-card-product',
-  imports: [MatCardModule, MatChipsModule, MatButtonModule, CommonModule, MatProgressSpinner],
+  imports: [MatCardModule, MatChipsModule, CommonModule],
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.css'
 })
 @Injectable({providedIn: 'root'})
 export class CardProductComponent {
   @Input() product!: any
-  @Input() isBtnPurchaseDisabled: boolean = false
-  private http = inject(HttpClient);
-  isPurchased: boolean = false
-  private apiUrl = environment.apiUrl
   loading = false
   isMobile = false;
-  insufficientFunds = false
 
-  constructor(private screenService: ScreenService, private snackbarService: SnackbarService) {}
+  constructor(
+    private screenService: ScreenService,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.screenService.isMobile$.subscribe(isMobile => {
@@ -34,43 +30,14 @@ export class CardProductComponent {
     });
   }
 
-  purchase(): void {
-    this.loading = true
-    const token = localStorage.getItem('token'); // Replace 'token' with the actual key you use.
+  navigateToProduct(): void {
+    const token = localStorage.getItem('token');
     if (token) {
-      const decodedToken = this.decodeToken(token);
-      const userId = decodedToken?.id; // Replace 'user_id' with the actual key in the payload.
-      const data = {
-        "idUser": userId,
-        "idProduct": this.product._id
-      }
-      this.http.post<any>(`${this.apiUrl}/api/purchases`, data).subscribe({
-        next: response => {
-          this.isPurchased = true
-          this.snackbarService.displaySuccess(`You bought ${this.product.name}!`)
-          this.loading = false
-        },
-        error: error => {
-          const errorMessage = error.error.error
-          this.snackbarService.displayError("Error: " + errorMessage)
-          this.loading = false
-          if (errorMessage === 'Insufficient funds')
-            this.insufficientFunds = true
-        },
-        complete: () => {
-          this.loading = false
-        }
-      });
-    }
-  }
-
-  decodeToken(token: string): any {
-    try {
-      const payload = token.split('.')[1];
-      return JSON.parse(atob(payload));
-    } catch (error) {
-      console.error('Failed to decode token:', error);
-      return null;
+      this.productService.setCurrentProduct(this.product)
+      this.router.navigate([`products/${this.product._id}`]);
+    } else {
+      console.error('No token found in localStorage.');
+      this.router.navigate(['/login']);
     }
   }
 
