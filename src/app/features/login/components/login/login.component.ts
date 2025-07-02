772436bbbeb8a543a6e2ffd1a82a768c8951601b
pedrochatelain <pedrochatelain@gmail.com>
2025-07-02 18:32:30 -0300

    refactor login page

--- src/app/features/login/components/login/login.component.ts
+++ src/app/features/login/components/login/login.component.ts
@@ -5,6 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { ScreenService } from '../../../../shared/services/screen.service';
 import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../../../../shared/snackbar/services/snackbar.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 /**
  * @title Basic Inputs
@@ -13,17 +16,18 @@ import { LoginService } from '../../services/login.service';
   selector: 'app-login',
   styleUrl: 'login.component.css',
   templateUrl: 'login.component.html',
-  imports: [LoginFormComponent, CreateAccountFormComponent, MatButtonModule, CommonModule],
+  imports: [LoginFormComponent, CreateAccountFormComponent, MatButtonModule, CommonModule, MatProgressSpinner],
 })
 export class LoginComponent {
-  showLoginForm = true;
-  toggle_forms_text = "I don't have an account"
-  isCreateUserHidden: boolean = true;
-  @ViewChild(CreateAccountFormComponent) createAccountComponent!: CreateAccountFormComponent;
   isMobile = false;
   loggingIn: boolean = false;
   
-  constructor(private screenService: ScreenService, private loginService: LoginService) {}
+  constructor(
+    private screenService: ScreenService,
+    private loginService: LoginService,
+    private router: Router,
+    private snackbarService: SnackbarService
+  ) {}
 
   ngOnInit(): void {
     this.screenService.isMobile$.subscribe(isMobile => {
@@ -32,9 +36,37 @@ export class LoginComponent {
     this.loginService.loggingIn.subscribe((value) => this.loggingIn = value)
   }
 
-  toggleForms() {
-    this.showLoginForm = !this.showLoginForm;
-    this.isCreateUserHidden = !this.isCreateUserHidden
-    this.createAccountComponent.emptyForm()  
-  }  
+
+  navigateToLoginWithMyAccount() {
+    this.router.navigate(['login-with-account'])
+  }
+
+  navigateToCreateAccount() {
+    this.router.navigate(['create-account'])
+  }
+
+  loginAsAdmin() {
+    this.loggingIn = true
+    this.loginService.loggingIn.emit(true)
+    this.loginService.login("admin", "admin").subscribe({
+      next: response => {
+        this.router.navigate(['home'])
+        this.loginService.setToken(response.token)
+        this.loggingIn = false
+        this.loginService.loggingIn.emit(false)
+        this.snackbarService.displaySuccess("Login successful")
+      },
+      error: error => {
+        const messageError = error.error.error
+        this.loggingIn = false
+        this.snackbarService.displayError(messageError)
+        this.loginService.loggingIn.emit(false)
+      },
+      complete: () => {
+        this.loggingIn = false
+        this.loginService.loggingIn.emit(false)
+      }
+    });
+  }
+
 }
